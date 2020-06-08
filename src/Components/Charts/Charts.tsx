380d124628dbdc80f813bbb546c2ea3dbb92f5b9
betanyeli <betangelii@gmail.com>
2020-06-08 19:50:02 -0400

    enviroment variables added

--- src/Components/Charts/Charts.tsx
+++ src/Components/Charts/Charts.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import moment from 'moment';
+import axios from 'axios';
+import { Button } from 'react-bootstrap'
 
 
 interface DateConstructor {
     StartDate: any
     EndDate: any,
-
-    
-
 }
+
+const apiInput = process.env.REACT_APP_API_INPUT;
+const apiKey = process.env.REACT_APP_API_KEY;
 
 export class Charts extends React.Component<{}, DateConstructor> {
     constructor(props: any) {
@@ -16,19 +18,26 @@ export class Charts extends React.Component<{}, DateConstructor> {
         this.state = {
             StartDate: new Date(),
             EndDate: new Date(),
-   
-
 
         }
         this.onChangeStart = this.onChangeStart.bind(this);
         this.onChangeEnd = this.onChangeEnd.bind(this);
+        this.getData = this.getData.bind(this)
+    }
+
+    getData() {
+        axios.get(`${apiInput}2010/01/dias_i/04/2010/01/dias_f/05?apikey=${apiKey}&formato=json`)
+            .then(res => {
+                const result = res.data;
+                console.log("result", result)
+            })
+            .catch(err => console.log(err))
+
     }
 
     onChangeStart(e: any) {
-        this.setState({ StartDate: e.target.value})
+        this.setState({ StartDate: e.target.value })
         console.log("Start", e.target.value);
-
-
 
     }
 
@@ -39,12 +48,13 @@ export class Charts extends React.Component<{}, DateConstructor> {
     }
 
     render() {
-       const { StartDate, EndDate} = this.state
+        const { StartDate, EndDate } = this.state
 
         return (
             <React.Fragment>
-                <input type="date" value= {StartDate} onChange={this.onChangeStart}></input>
-                <input type="date" value= {EndDate} onChange={this.onChangeEnd}></input>
+                <input type="date" value={StartDate} onChange={this.onChangeStart}></input>
+                <input type="date" min={StartDate} value={EndDate} onChange={this.onChangeEnd}></input>
+                <Button onClick={this.getData} variant="primary">Click me</Button>
 
             </React.Fragment>
         )
