584804e2f6663d64db81f60aa03b233c115ddc7f
betanyeli <betangelii@gmail.com>
2020-06-09 23:29:10 -0400

    adding current indicators in navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,15 +1,73 @@
 import React from 'react';
-import { Navbar} from 'react-bootstrap';
-import {Typewriter} from 'typewriter-effect';
+import { Navbar, Nav } from 'react-bootstrap';
+import moment from 'moment';
+import 'moment/locale/es';
+import axios from 'axios';
 import './Styles.css'
 
-export class Appbar extends React.Component {
+interface NavConstructor {
+  Params: any,
+  Dollar: any,
+  Euro: any,
+  UFs: any
+
+}
+
+const apiInput: any = process.env.REACT_APP_DEFAULT_INPUT
+const apiKey: any = process.env.REACT_APP_API_KEY
+
+export class Appbar extends React.Component<{}, NavConstructor> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      Params: ["dolar", "euro", "uf"],
+      Dollar: '',
+      Euro: '',
+      UFs: ''
+    }
+  }
+
+  componentDidMount() {
+    let dollarQuery: string = `${apiInput}${this.state.Params[0]}?apikey=${apiKey}&formato=json`
+    let euroQuery: string = `${apiInput}${this.state.Params[1]}?apikey=${apiKey}&formato=json`
+    let ufQuery: string = `${apiInput}${this.state.Params[2]}?apikey=${apiKey}&formato=json`
+
+    axios.get(dollarQuery).then(res => { this.setState({ Dollar: res.data.Dolares[0] }) })
+      .catch(err => { console.log(err) })
+
+    axios.get(euroQuery).then(res => { this.setState({ Euro: res.data.Euros[0] }) })
+      .catch(err => { console.log(err) })
+
+    axios.get(ufQuery).then(res => { this.setState({ UFs: res.data.UFs[0] }) })
+      .catch(err => { console.log(err) })
+
+  }
+
   render() {
+    const { Dollar, Euro, UFs } = this.state;
+    console.log(Dollar, Euro, UFs)
     return (
-<Navbar collapseOnSelect expand="lg" className="custom-nav">
-  <Navbar.Brand href="#home">SBIF</Navbar.Brand>
-  
-</Navbar>
+      <Navbar collapseOnSelect expand="lg" className="custom-nav">
+        <Navbar.Brand href="#home">SBIF</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="collapse">
+          <Nav className="mr-auto">
+            <Nav.Link >
+              Indicadores al {moment().format("LL")} :
+            </Nav.Link>
+            <Nav.Link >
+              Dólar: {Dollar.Valor}
+            </Nav.Link>
+            <Nav.Link >
+              Euro: {Euro.Valor}
+            </Nav.Link>
+            <Nav.Link >
+              UF: {UFs.Valor}
+            </Nav.Link>
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
